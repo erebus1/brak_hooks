aa1dd071db891961166b6c0c4d95ed02ccde96ce
Bartek Brak <bartek.rychlicki@gmail.com>
2016-01-10 14:03:34 +0100

    Add some isort version checking, fail on wrong versions.
    This should be changed with isort==4.2.3 which introduces
    setting virtual_env

--- brak_hooks.py
+++ brak_hooks.py
@@ -1,3 +1,6 @@
+import os
+
+
 def isort_hook(ui, repo, hooktype, node, **kwargs):
     try:
         import isort
@@ -5,6 +8,27 @@ def isort_hook(ui, repo, hooktype, node, **kwargs):
         ui.warn('install isort (outside virtual env) to run this hook')
         return True  # Failure
 
+    if '3.9.5' < isort.__version__ < '4.2.3':
+        if not os.environ.get('VIRTUAL_ENV'):
+            ui.warn('isort_hook can only be run from CLI, with virtual env enabled\n\n')
+            return True
+    elif isort.__version__ >= '4.2.3':
+        isort_config = isort.SortImports().config
+        virtual_env = isort_config.get('virtual_env') or os.environ.get('VIRTUAL_ENV')
+        if not virtual_env:
+            ui.warn(
+                'This hook requires you to either:\n\n'
+                'a) set path to your virtualenv explicitly in isort settings [*], for example:\n'
+                '# .isort.cfg'
+                '[settings]\n'
+                'virtual_env=/home/user/.pyenv/versions/2.7.10/envs/your_project\n\n'
+                'b) be in a virtualenv, this will work only for CLI, not your repo tools or IDE\n\n'
+                '[*] https://github.com/timothycrosley/isort/wiki/isort-Settings\n\n'
+            )
+            return True
+    else:
+        ui.warn('isort_hook requires isort>=3.9.5\n\n')
+        return True
     errors = 0
     ctx = repo[node]
     if 'no-isort' in ctx.description():
--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     py_modules=['brak_hooks'],
     author='Bartek Brak',
     author_email='bartek.rychlicki@gmail.com',
-    install_requires=['isort']
+    # 3.9.5 introduced checking VIRTUAL_ENV
+    install_requires=['isort>=3.9.5']
 )
